067d3d5cc5ca5e22c10be78f4fe65411fa955a70
Fuze <softrosypapers@gmail.com>
2026-09-10 00:22:22 +0530

    Added direct secret file path for render

--- server/firebase_config.py
+++ server/firebase_config.py
@@ -3,9 +3,16 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-key_dict = json.loads(os.getenv("FIREBASE_KEY_JSON"))
-cred = credentials.Certificate(key_dict)
 
+if os.path.exists("/etc/secrets/service-account-key.json"):
+    secret_file_path = "/etc/secrets/service-account-key.json"
+else:
+    secret_file_path = "service-account-key.json"
+
+with open(secret_file_path, "r", encoding="utf-8") as file:
+    key_dict = json.load(file)
+
+cred = credentials.Certificate(key_dict)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
